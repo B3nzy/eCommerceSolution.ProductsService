195842aa93404ad9c36be48192e772332d9fc0a4
Sumit Mandal <sumit.mandal0123@gmail.com>
2026-05-10 15:48:17 +0530

    add gloobal exception handling middleware

--- eCommerceSolution.ProductsService/Program.cs
+++ eCommerceSolution.ProductsService/Program.cs
@@ -1,3 +1,4 @@
+using eCommerceSolution.ProductsService.Middlewares;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandlingMiddleware();
 
 app.UseAuthorization();
 
